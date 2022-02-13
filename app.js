1e8669772e16dd823f2ff2f159d693dde8e243d9
545n shahin <mohammadshahinkhan1992@gmail.com>
2022-02-13 18:49:43 +0800

    first commit

--- app.js
+++ app.js
@@ -1,29 +1,40 @@
-function updateCaseNumber(isIncreasing){
-    const caseInput = document.getElementById('case-number');
-    let caseNumber = caseInput.value;
+function updateProductNumber(product, price, isIncreasing){
+    const productInput = document.getElementById(product + '-number');
+    let productNumber = productInput.value;
     // console.log(caseNumber);
     if(isIncreasing == true){
-        caseNumber = parseInt( caseNumber) + 1;
+        productNumber = parseInt( productNumber) + 1;
     }
-    else if (caseNumber > 0){
-        caseNumber = parseInt(caseNumber) - 1 ;
+    else if (productNumber > 0){
+        productNumber = parseInt(productNumber) - 1 ;
 
     }
-    caseInput.value = caseNumber;
+    productInput.value = productNumber;
 
     // update case total 
-    const caseTotal = document.getElementById('case-total');
-    caseTotal.innerText = caseNumber * 59;
+    const productTotal = document.getElementById(product + '-total');
+    productTotal.innerText = productNumber * price;
 }
+// phone increas decrease events
+document.getElementById('phone-plus').addEventListener('click', function(){
+    console.log('click phone increase');
+    updateProductNumber('phone', 1229, true);
+});
+document.getElementById('phone-minus').addEventListener('click', function(){
+    updateProductNumber('phone', 1219, false);
+})
 
 
+
+
+// handle case increase decrease events
 document.getElementById('case-plus').addEventListener('click', function(){
     // console.log('case plus click')
     /* const caseInput = document.getElementById('case-number');
     const caseNumber = casseInput.value;
     // console.log(caseNumber);
     caseInput.value = parseInt( caseNumber) + 1; */
-    updateCaseNumber(true);
+    updateProductNumber('case', 59, true);
 
 })
 
@@ -31,6 +42,6 @@ document.getElementById('case-minus').addEventListener('click', function(){
     // console.log('minus button is clicked')
     /* const caseInput = document.getElementById('case-number');
     const caseNumber = caseInput.value; */
-    updateCaseNumber(false);
+    updateProductNumber('case', 59, false);
     
 })
